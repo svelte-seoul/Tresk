93f23f5473fc99d9ad3bb4933cb73b719ddb0d5d
yujong-lee <yujonglee@g.skku.edu>
2021-10-16 15:47:40 +0900

    Add missing async/await in [AddTask]

--- src/components/AddTask.test.ts
+++ src/components/AddTask.test.ts
@@ -17,12 +17,12 @@ describe('AddTask', () => {
     expect(getByRole('button', { name: 'Add' })).toBeInTheDocument();
   });
 
-  it('adds task to taskStolage on click', () => {
+  it('adds task to taskStolage on click', async () => {
     const { getByRole } = render(AddTask);
 
     selectedId.set(0);
     inputField.set('task 1');
-    fireEvent.click(getByRole('button', { name: 'Add' }));
+    await fireEvent.click(getByRole('button', { name: 'Add' }));
 
     expect(get(taskStolage)).toEqual({
       0: { main: 'base', subTasks: [1] },
@@ -31,7 +31,7 @@ describe('AddTask', () => {
 
     selectedId.set(1);
     inputField.set('task 2');
-    fireEvent.click(getByRole('button', { name: 'Add' }));
+    await fireEvent.click(getByRole('button', { name: 'Add' }));
 
     expect(get(taskStolage)).toEqual({
       0: { main: 'base', subTasks: [1] },
