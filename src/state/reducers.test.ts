8a11d95f658b7b02a1579274e2d13deb3661795c
yujong-lee <yujonglee@g.skku.edu>
2021-10-19 06:19:12 +0900

    Refactor [reducer] test

--- src/state/reducers.test.ts
+++ src/state/reducers.test.ts
@@ -1,42 +1,37 @@
-import type { Content, TaskStorage } from '../types';
+import type { TaskStorage } from '../types';
 
 import { add, remove } from './reducers';
 
 describe('add', () => {
   it('adds new task to list', () => {
-    const parent = 1;
-    const content: Content = 'content 4';
-
     const before: TaskStorage = {
+      0: { main: 'base', subTasks: [1] },
       1: { main: 'content 1', subTasks: [] },
-      3: { main: 'content 3', subTasks: [] },
     };
 
     const after: TaskStorage = {
-      1: { main: 'content 1', subTasks: [4] },
-      3: { main: 'content 3', subTasks: [] },
-      4: { main: 'content 4', subTasks: [] },
+      0: { main: 'base', subTasks: [1] },
+      1: { main: 'content 1', subTasks: [2] },
+      2: { main: 'content 2', subTasks: [] },
     };
 
-    expect(add(before, parent, content)).toEqual(after);
+    expect(add(before, 1, 'content 2')).toEqual(after);
   });
 });
 
 describe('remove', () => {
   it('removes task from list', () => {
-    const target: number = 3;
-
     const before: TaskStorage = {
-      1: { main: 'content 1', subTasks: [3] },
+      0: { main: 'base', subTasks: [1] },
+      1: { main: 'content 1', subTasks: [2] },
       2: { main: 'content 2', subTasks: [] },
-      3: { main: 'content 3', subTasks: [] },
     };
 
     const after: TaskStorage = {
+      0: { main: 'base', subTasks: [1] },
       1: { main: 'content 1', subTasks: [] },
-      2: { main: 'content 2', subTasks: [] },
     };
 
-    expect(remove(before, target)).toEqual(after);
+    expect(remove(before, 2)).toEqual(after);
   });
 });
